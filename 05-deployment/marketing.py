8dd465324dcc11bfd467c1a7c5fdc8c5c3a89c34
DavidVFitzGerald <72464297+DavidVFitzGerald@users.noreply.github.com>
2025-10-24 11:08:35 +0000

    Add marketing script

---
+++ 05-deployment/marketing.py
@@ -0,0 +1,35 @@
+import requests
+
+url = "http://localhost:9696/predict"
+customer = {
+    "gender": "male",
+    "seniorcitizen": 0,
+    "partner": "no",
+    "dependents": "no",
+    "phoneservice": "yes",
+    "multiplelines": "no",
+    "internetservice": "dsl",
+    "onlinesecurity": "yes",
+    "onlinebackup": "yes",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "mailed_check",
+    "tenure": 6,
+    "monthlycharges": 23.85,
+    "totalcharges": 263.55
+}
+
+response = requests.post(url, json=customer)
+churn  = response.json()
+
+print(f"Probability of churning = {churn['churn_probability']}")
+print(f"Churn = {churn['churn']}")
+
+if churn['churn_probability'] >= 0.5:
+    print("Send email with promo")
+else:
+    print("Don't do anything")
