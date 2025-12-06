0fa3998add75b9d8b604086a642dacf91bf57562
David FitzGerald <dvfitzgerald@hotmail.com>
2025-12-06 18:58:32 +0700

    Add module 09 files for sklearn model

---
+++ 09-serverless/sklearn/publish.sh
@@ -0,0 +1,21 @@
+
+
+ECR_URL=439440559013.dkr.ecr.ap-southeast-2.amazonaws.com
+REPO_URL=${ECR_URL}/churn-prediction-lambda
+REMOTE_IMAGE_TAG="${REPO_URL}:v1"
+
+LOCAL_IMAGE=churn-prediction-lambda
+
+
+aws ecr get-login-password \
+  --region "ap-southeast-2" \
+| docker login \
+  --username AWS \
+  --password-stdin ${ECR_URL}
+
+
+docker build -t ${LOCAL_IMAGE} .
+docker tag ${LOCAL_IMAGE} ${REMOTE_IMAGE_TAG}
+docker push ${REMOTE_IMAGE_TAG}
+
+echo "Done"
